8dbefd4e6a3e61ef3e1fd8346cc75e48b4a2b0f1
Val <valtodus@gmail.com>
2024-01-11 23:14:03 +0200

    нов тест

---
+++ testGitHub/testGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace testGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
